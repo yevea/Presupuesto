d821674a96cc636d5bdd91d6960ec929899b042d
tableros <40235617+yevea@users.noreply.github.com>
2026-02-02 14:48:25 +0100

    Update Presupuesto.php

--- Presupuesto.php
+++ Presupuesto.php
@@ -1,1 +1,24 @@
+<?php
+/**
+ * Presupuesto - Simple WooCommerce Sync Plugin
+ */
 
+namespace FacturaScripts\Plugins\Presupuesto;
+
+use FacturaScripts\Core\Base\CronClass;
+use FacturaScripts\Core\Tools;
+
+class Presupuesto extends CronClass
+{
+    public function __construct()
+    {
+        $this->setPeriod(900); // 15 minutes
+    }
+
+    public function run(): void
+    {
+        if (Tools::settings('Presupuesto', 'enable_sync', false)) {
+            Tools::log()->info('Presupuesto: Auto-sync running');
+        }
+    }
+}
